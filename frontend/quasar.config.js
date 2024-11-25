61cf5ab8c516387364f76ad7db0f75ae62409da8
Enric Pou <enricpou@gmail.com>
2024-11-25 15:46:25 +0100

    Prepare for gh-pages deploy

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -53,7 +53,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: process.env.NODE_ENV === "production" ? "/anomaly_detection/" : "/",
       // analyze: true,
       // env: {},
       // rawDefine: {}
