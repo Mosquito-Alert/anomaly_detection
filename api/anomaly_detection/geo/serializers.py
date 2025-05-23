6d8bf8ef431cb0bc167bacc5d0a397c6e3311ff9
Guillermo Sanz <guillermo.sanz@ceab.csic.es>
2025-05-23 11:14:21 +0200

    [API] Resolved bug which returned the province name outside of the region object instead of inside.

--- api/anomaly_detection/geo/serializers.py
+++ api/anomaly_detection/geo/serializers.py
@@ -1,5 +1,6 @@
 
 
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from anomaly_detection.geo.models import Municipality
@@ -9,7 +10,16 @@ class MunicipalitySerializer(ModelSerializer):
     """
     Serializer for the Municipality model.
     """
+    province = SerializerMethodField()
+
+    def get_province(self, obj=None) -> str:
+        """
+        Get the province name from the province related model.
+        """
+        if obj and obj.province:
+            return obj.province.name
+        return None
 
     class Meta:
         model = Municipality
-        fields = ['code', 'name', 'alt_name']
+        fields = ['code', 'name', 'alt_name', 'province']
