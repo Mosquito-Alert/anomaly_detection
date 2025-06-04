ad292864016578b0741889253439d064c4b3ea36
Guillermo Sanz <guillermo.sanz@ceab.csic.es>
2025-06-04 13:00:20 +0200

    remove hosts for production

--- api/project/hosts.py
+++ api/project/hosts.py
@@ -3,5 +3,5 @@ from django_hosts import patterns, host
 
 host_patterns = patterns(
     '',
-    host(r'api', settings.ROOT_URLCONF, name='api'),
+    # host(r'api', settings.ROOT_URLCONF, name='api'),
 )
